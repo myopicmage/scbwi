3e4c458a5f38cf6089b88207388ef587b5fa5aa3
Kevin Bernfeld <kcbernfeld@gmail.com>
2017-08-10 11:56:26 -0400

    adding yarn

--- scbwi/Controllers/ApiController.cs
+++ scbwi/Controllers/ApiController.cs
@@ -8,37 +8,34 @@ using Braintree;
 using scbwi.Models;
 using Microsoft.Extensions.Options;
 
-namespace scbwi.Controllers
-{
-    public class ApiController : Controller
-    {
+namespace scbwi.Controllers {
+    public class ApiController : Controller {
         private readonly BraintreeGateway _gateway;
         private readonly ILogger _logger;
         private readonly ScbwiContext _db;
         //private readonly IEmailSender _email;
         //private readonly ITotalCalculator _calc;
 
-        public ApiController(ScbwiContext db, ILoggerFactory factory, IOptions<Secrets> secrets)
-        {
+        public ApiController(ScbwiContext db, ILoggerFactory factory, IOptions<Secrets> secrets) {
             _db = db;
             _logger = factory.CreateLogger("All");
             _gateway = new BraintreeGateway(secrets.Value.paypaltoken);
         }
 
-        public IActionResult GetToken()
-        {
-            try
-            {
+        public IActionResult GetToken() {
+            try {
                 var token = _gateway.ClientToken.generate();
 
                 return Json(new { token = token });
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 _logger.LogCritical($"Failed to generate token! {ex.Message}");
 
                 throw;
             }
+        }
+
+        public async Task<IActionResult> Register([FromBody] BootcampRegisration r) {
+            return Json(true);
         }
     }
 }
